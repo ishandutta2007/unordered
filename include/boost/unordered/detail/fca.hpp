4d49eca28c38dc316cb0769fd6903c2e4006d735
Christian Mazakas <christian.mazakas@gmail.com>
2022-05-09 10:19:07 -0700

    Update copyright for files affected by the FCA upgrade

--- include/boost/unordered/detail/fca.hpp
+++ include/boost/unordered/detail/fca.hpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2022 Joaquin M Lopez Munoz.
+// Copyright (C) 2022 Christian Mazakas
+//
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
